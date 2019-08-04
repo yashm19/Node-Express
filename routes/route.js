5cae4cb8045be7a04ba68041a22a006c6cf6581a
Sahil Arora <mamidwaryash@gmail.com>
2019-08-04 16:10:35 +0530

    router change

--- routes/route.js
+++ routes/route.js
@@ -3,5 +3,6 @@ var Employees =  require("../Controllers/Employee");
 var router = express.Router();
 
 router.get("/Employees",Employees.getAll);
+console.log("express");
 
 module.exports =  router
